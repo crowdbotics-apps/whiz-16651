a0fdfe61e63cc7458efae5524b4214df41dda091
Crowdbotics <team@crowdbotics.com>
2020-05-06 02:24:44 +0000

    Initial commit.

---
+++ backend/whiz_16651/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for whiz_16651 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whiz_16651.settings')
+
+application = get_wsgi_application()
